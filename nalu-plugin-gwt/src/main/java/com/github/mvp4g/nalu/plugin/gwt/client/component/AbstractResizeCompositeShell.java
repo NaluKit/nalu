b5d28127b3aa0a27c22d102862acdec469e0b6b0
Frank Hossfeld <hoss@El-Hoss-MacBook-Pro.local>
2018-09-13 19:57:02 +0200

    update documentation

---
+++ nalu-plugin-gwt/src/main/java/com/github/mvp4g/nalu/plugin/gwt/client/component/AbstractResizeCompositeShell.java
@@ -0,0 +1,42 @@
+package com.github.mvp4g.nalu.plugin.gwt.client.component;
+
+import com.github.mvp4g.nalu.client.Router;
+import com.github.mvp4g.nalu.client.application.IsContext;
+import com.github.mvp4g.nalu.client.component.IsShell;
+import com.google.gwt.user.client.ui.ResizeComposite;
+import org.gwtproject.event.shared.SimpleEventBus;
+
+public abstract class AbstractResizeCompositeShell<C extends IsContext>
+    extends ResizeComposite
+    implements IsShell {
+
+  protected Router router;
+
+  protected C context;
+
+  protected SimpleEventBus eventBus;
+
+  public AbstractResizeCompositeShell() {
+  }
+
+  public void setRouter(Router router) {
+    this.router = router;
+  }
+
+  public void setContext(C context) {
+    this.context = context;
+  }
+
+  public void setEventBus(SimpleEventBus eventBus) {
+    this.eventBus = eventBus;
+  }
+
+  public void bind() {
+    // override this method if you need to bind something inside the Shell
+  }
+
+  @Override
+  public void onAttachedComponent() {
+    // override this method if you need to do something, after a component is attached!
+  }
+}
