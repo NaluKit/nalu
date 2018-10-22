7db3fe4bd7d5690702f85582669639d6f5fc0df9
Tino Desjardins <tino.desjardins@arcor.de>
2018-10-22 14:38:11 +0200

    Fix typo

--- nalu/src/main/java/com/github/nalukit/nalu/client/application/IsApplicationLoader.java
+++ nalu/src/main/java/com/github/nalukit/nalu/client/application/IsApplicationLoader.java
@@ -20,7 +20,7 @@ package com.github.nalukit.nalu.client.application;
  * Nalu application loader
  * <br>
  * <p>The Loader is executed during the start sequence of the application.
- * The loader can be used to load meta-informations at the the start of the applilcation</p>
+ * The loader can be used to load meta-informations at the the start of the application</p>
  * <p>Once the work is done call finishLoadCommand.finishLoad() to resume with the normal processing.</p>
  */
 public interface IsApplicationLoader<C extends IsContext> {
