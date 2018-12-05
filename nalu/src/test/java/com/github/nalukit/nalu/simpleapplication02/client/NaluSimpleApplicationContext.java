9a3c052ae388d8e6f70031ad27f86cf75e8bf8ce
Frank Hossfeld <frank.hossfeld@googlemail.com>
2018-12-05 08:32:37 +0100

    #20 Remove 'sudo: false' from travis.yml

--- nalu/src/test/java/com/github/nalukit/nalu/simpleapplication02/client/NaluSimpleApplicationContext.java
+++ nalu/src/test/java/com/github/nalukit/nalu/simpleapplication02/client/NaluSimpleApplicationContext.java
@@ -31,7 +31,7 @@ public class NaluSimpleApplicationContext
   private String searchCity;
 
   public NaluSimpleApplicationContext() {
-    this.version = "1.0.0";
+    this.version = "1.0.1-SNAPSHOT";
   }
 
   public String getVersion() {
