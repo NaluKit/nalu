8e37334103806069adb6ae9970941a483853ad4b
Frank Hossfeld <frank.hossfeld@googlemail.com>
2019-07-30 07:34:35 +0200

    fixed typo

--- nalu-processor/src/main/java/com/github/nalukit/nalu/processor/NaluProcessor.java
+++ nalu-processor/src/main/java/com/github/nalukit/nalu/processor/NaluProcessor.java
@@ -441,7 +441,7 @@ public class NaluProcessor
     this.processorUtils = ProcessorUtils.builder()
                                         .processingEnvironment(processingEnv)
                                         .build();
-    // get stored MEta Model and use it, if there is one!
+    // get stored Meta Model and use it, if there is one!
     MetaModel restoredModel = this.restore();
     if (!Objects.isNull(restoredModel)) {
       this.metaModel = restoredModel;
