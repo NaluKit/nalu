5ec282db39d7cb35bdfa5a43562206f13b2608e8
Frank Hossfeld <frank.hossfeld@googlemail.com>
2019-08-06 21:02:05 +0200

    update version to 1.3.2-SNAPSHOT

--- nalu-processor/src/main/java/com/github/nalukit/nalu/processor/ProcessorVersion.java
+++ nalu-processor/src/main/java/com/github/nalukit/nalu/processor/ProcessorVersion.java
@@ -2,6 +2,6 @@ package com.github.nalukit.nalu.processor;
 
 public class ProcessorVersion {
 
-  public final static String PROCESSOR_VERSION = "1.3.1";
+  public final static String PROCESSOR_VERSION = "1.3.2-SNAPSHOT";
 
 }
--- nalu/src/main/java/com/github/nalukit/nalu/client/Nalu.java
+++ nalu/src/main/java/com/github/nalukit/nalu/client/Nalu.java
@@ -22,7 +22,7 @@ import com.github.nalukit.nalu.client.internal.route.RouterUtils;
 public class Nalu {
 
   public static String getVersion() {
-    return "1.3.1";
+    return "1.3.2-SNAPSHOT";
   }
 
   public static boolean hasHistory() {
