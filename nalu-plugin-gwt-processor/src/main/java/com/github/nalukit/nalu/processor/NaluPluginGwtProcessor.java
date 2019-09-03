8053cdd7e9d0551b0adde361c0b4bc380307184b
Frank Hossfeld <fran.hossfeld@googlemail.com>
2019-09-03 09:52:05 +0200

    update version to 1.3.2

--- nalu-plugin-gwt-processor/src/main/java/com/github/nalukit/nalu/processor/NaluPluginGwtProcessor.java
+++ nalu-plugin-gwt-processor/src/main/java/com/github/nalukit/nalu/processor/NaluPluginGwtProcessor.java
@@ -60,7 +60,7 @@ public class NaluPluginGwtProcessor
     this.stopwatch = Stopwatch.createStarted();
     setUp();
     this.processorUtils.createNoteMessage("Nalu-Plugin-GWT-Processor started ...");
-    this.processorUtils.createNoteMessage("Nalu-Plugin-GWT-Processor version >>1.3.2-SNAPSHOT<<");
+    this.processorUtils.createNoteMessage("Nalu-Plugin-GWT-Processor version >>1.3.2<<");
   }
 
   @Override
