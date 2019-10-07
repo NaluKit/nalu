9abd473bf4df77002c211f07fc7e4e481f73a0d0
Harald Pehl <harald.pehl@gmail.com>
2019-10-07 14:33:05 +0200

    Add Nalu Elemento plugin

---
+++ nalu-plugin-elemento/src/main/java/com/github/nalukit/nalu/plugin/elemento/client/DefaultElemental2Logger.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018 - 2019 - Frank Hossfeld
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+package com.github.nalukit.nalu.plugin.elemento.client;
+
+import com.github.nalukit.nalu.plugin.core.web.client.AbstractLogger;
+import com.github.nalukit.nalu.plugin.core.web.client.NaluPluginCoreWeb;
+import elemental2.dom.DomGlobal;
+
+public class DefaultElemental2Logger
+    extends AbstractLogger {
+
+  static final String INDENT = "..";
+
+  public void log(String message,
+                  int depth) {
+    if (NaluPluginCoreWeb.isSuperDevMode()) {
+      DomGlobal.window.console.log(createLog(message,
+                                             depth));
+    }
+  }
+
+}
