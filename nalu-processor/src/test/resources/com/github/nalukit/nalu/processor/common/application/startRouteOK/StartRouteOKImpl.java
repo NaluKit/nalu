d315a496b1e6a6092aafc2c1d34f4abd5a72d47b
Frank Hossfeld <frank.hossfeld@googlemail.com>
2021-06-17 19:12:07 +0200

    set version to HEAD-SNAPSHOT-gwt-2.8.2

--- nalu-processor/src/test/resources/com/github/nalukit/nalu/processor/common/application/startRouteOK/StartRouteOKImpl.java
+++ nalu-processor/src/test/resources/com/github/nalukit/nalu/processor/common/application/startRouteOK/StartRouteOKImpl.java
@@ -15,7 +15,7 @@ import java.lang.Override;
 import java.util.Arrays;
 
 /**
- * Build with Nalu version >>2.5.0-gwt-2.8.2<< at >>2021.03.31-21:44:59<<
+ * Build with Nalu version >>HEAD-SNAPSHOT-gwt-2.8.2<< at >>2021.03.31-21:44:59<<
  */
 public final class StartRouteOKImpl extends AbstractApplication<MockContext> implements StartRouteOK {
   public StartRouteOKImpl() {
@@ -29,7 +29,7 @@ public final class StartRouteOKImpl extends AbstractApplication<MockContext> imp
 
   @Override
   public void logProcessorVersion() {
-    this.eventBus.fireEvent(LogEvent.create().sdmOnly(true).addMessage("=================================================================================").addMessage("Nalu processor version  >>2.5.0-gwt-2.8.2<< used to generate this source").addMessage("=================================================================================").addMessage(""));
+    this.eventBus.fireEvent(LogEvent.create().sdmOnly(true).addMessage("=================================================================================").addMessage("Nalu processor version  >>HEAD-SNAPSHOT-gwt-2.8.2<< used to generate this source").addMessage("=================================================================================").addMessage(""));
   }
 
   @Override
--- nalu/src/main/java/com/github/nalukit/nalu/client/Nalu.java
+++ nalu/src/main/java/com/github/nalukit/nalu/client/Nalu.java
@@ -22,7 +22,7 @@ import com.github.nalukit.nalu.client.internal.route.RouterUtils;
 public class Nalu {
   
   public static String getVersion() {
-    return "2.5.0-gwt-2.8.2";
+    return "HEAD-SNAPSHOT-gwt-2.8.2";
   }
   
   public static boolean hasHistory() {
