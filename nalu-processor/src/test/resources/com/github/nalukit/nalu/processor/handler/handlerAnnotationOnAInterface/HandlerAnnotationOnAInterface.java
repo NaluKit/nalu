65a0b1ee63c07c19f44b4c2ee8b2fbe421020d6a
philkohl <65112414+philkohl@users.noreply.github.com>
2022-03-20 13:05:27 +0100

    chore: :wrench: Cherry pick cleaning step for nalu processor test resources (#257)
    Done

--- nalu-processor/src/test/resources/com/github/nalukit/nalu/processor/handler/handlerAnnotationOnAInterface/HandlerAnnotationOnAInterface.java
+++
@@ -1,9 +0,0 @@
-package com.github.nalukit.nalu.processor.handler.handlerAnnotationOnAInterface;
-
-import com.github.nalukit.nalu.client.handler.AbstractHandler;
-import com.github.nalukit.nalu.client.handler.annotation.Handler;
-import com.github.nalukit.nalu.processor.common.MockContext;
-
-@Handler
-public interface HandlerAnnotationOnAInterface {
-}
