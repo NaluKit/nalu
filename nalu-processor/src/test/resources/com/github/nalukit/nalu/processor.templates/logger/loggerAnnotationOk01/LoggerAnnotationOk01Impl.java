1618dab1c7b9b005aef0e43e6a76807c01a3d9ff
Frank Hossfeld <frank.hossfeld@googlemail.com>
2022-06-11 14:18:35 +0200

    #266 - Logger does not respect 'sdmOnly' in case of using a custom logger - fixed a test issue

--- nalu-processor/src/test/resources/com/github/nalukit/nalu/processor.templates/logger/loggerAnnotationOk01/LoggerAnnotationOk01Impl.java
+++ nalu-processor/src/test/resources/com/github/nalukit/nalu/processor.templates/logger/loggerAnnotationOk01/LoggerAnnotationOk01Impl.java
@@ -28,7 +28,7 @@ public final class LoggerAnnotationOk01Impl extends AbstractApplication<MockCont
   @Override
   public void loadLoggerConfiguration() {
     MockDebug clientLogger = new MockDebug();
-    this.naluLogger.setClientLogger(clientLoggerZ);
+    this.naluLogger.setClientLogger(clientLogger);
     MockLogger customLogger = new MockLogger();
     customLogger.setContext(this.context);
     this.naluLogger.setCustomLogger(customLogger);
