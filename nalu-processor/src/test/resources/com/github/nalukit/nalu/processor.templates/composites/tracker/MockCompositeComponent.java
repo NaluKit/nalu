03252fe67c1c55266e8169329b9da954e9002758
Frank Hossfeld <frank.hossfeld@googlemail.com>
2022-10-12 13:25:57 +0200

    #277 - @Composites-annotation can be added to other classes as controllers - done (#282)

---
+++ nalu-processor/src/test/resources/com/github/nalukit/nalu/processor.templates/composites/tracker/MockCompositeComponent.java
@@ -0,0 +1,21 @@
+package com.github.nalukit.nalu.processor.composites.tracker;
+
+import com.github.nalukit.nalu.client.component.AbstractCompositeComponent;
+import elemental2.dom.HTMLDivElement;
+import elemental2.dom.HTMLElement;
+
+import static elemental2.dom.DomGlobal.document;
+
+public class MockCompositeComponent
+    extends AbstractCompositeComponent<IMockCompositeComponent.Controller, HTMLElement>
+    implements IMockCompositeComponent {
+
+  public MockCompositeComponent() {
+  }
+
+  @Override
+  public void render() {
+    HTMLDivElement divElemet = (HTMLDivElement) document.createElement("div");
+    initElement(divElemet);
+  }
+}
