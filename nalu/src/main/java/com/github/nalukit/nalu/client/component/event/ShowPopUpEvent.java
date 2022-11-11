5b07f684076f50bb6b78be29a3257e877516f39a
Frank Hossfeld <frank.hossfeld@googlemail.com>
2022-11-11 12:15:49 +0100

    #284 - NaluApplicationEvent: add 'commandStore' and 'dataStore' - done

--- nalu/src/main/java/com/github/nalukit/nalu/client/component/event/ShowPopUpEvent.java
+++ nalu/src/main/java/com/github/nalukit/nalu/client/component/event/ShowPopUpEvent.java
@@ -32,6 +32,13 @@ public class ShowPopUpEvent
     return new ShowPopUpEvent(name);
   }
 
+  /**
+   * Adds String-data to the String-data store.
+   *
+   * @param key   key of the parameter
+   * @param value value of the parameter
+   * @return instance of the event
+   */
   public ShowPopUpEvent using(String key,
                               String value) {
     this.dataStore.put(key,
@@ -39,6 +46,15 @@ public class ShowPopUpEvent
     return this;
   }
 
+  /**
+   * Adds data to the data store.
+   *
+   * <b>Keep in mind, all parameters will be stored as objects!</b>
+   *
+   * @param key   key of the parameter
+   * @param value value of the parameter
+   * @return instance of the event
+   */
   public ShowPopUpEvent usingObject(String key,
                                     Object value) {
     this.dataObjectStore.put(key,
@@ -46,6 +62,13 @@ public class ShowPopUpEvent
     return this;
   }
 
+  /**
+   * Adds a command to the command store.
+   *
+   * @param key     key of the parameter
+   * @param command command to store
+   * @return instance of the event
+   */
   public ShowPopUpEvent add(String key,
                             PopUpCommand command) {
     this.commandStore.put(key,
@@ -67,14 +90,29 @@ public class ShowPopUpEvent
     return name;
   }
 
+  /**
+   * Returns the command store
+   *
+   * @return the store containing key-value-pairs where value is a command
+   */
   public Map<String, PopUpCommand> getCommandStore() {
     return commandStore;
   }
 
+  /**
+   * Returns the data store where all String are stored.
+   *
+   * @return the store containing key-value-pairs where value is a String
+   */
   public Map<String, String> getDataStore() {
     return dataStore;
   }
 
+  /**
+   * Returns the data object store where all object are stored.
+   *
+   * @return the store containing key-value-pairs where value is an Object that needs to be cast
+   */
   public Map<String, Object> getDataObjectStore() {
     return dataObjectStore;
   }
