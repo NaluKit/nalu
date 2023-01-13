374d371c34247fad7b84cf63b58d9b6b36a7fae2
Frank Hossfeld <frank.hossfeld@googlemail.com>
2023-01-13 18:09:04 +0100

    housekeeping

--- nalu/src/main/java/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * The package 'org.gwtproject.event.shared' stays here a s long as gwt-events
- * is not released and not on Maven Central
- * <p>
- * Once it is finally released and added to Maven Central, the package 'org.gwtproject.event.shared'
- * will be removed and the classes replaced by the gwt-events artifact.
- */
