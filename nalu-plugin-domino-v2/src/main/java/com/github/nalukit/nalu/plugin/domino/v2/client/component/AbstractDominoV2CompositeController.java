de28dae8f13b37e9ee0a5e5a6953c465610e6bb1
Frank Hossfeld <frank.hossfeld@googlemail.com>
2023-08-05 10:11:46 +0200

    #329 - working with no hash, a '#' at the end reloads the application - rename `domino` -> `domino-v2`

---
+++ nalu-plugin-domino-v2/src/main/java/com/github/nalukit/nalu/plugin/domino/v2/client/component/AbstractDominoV2CompositeController.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 - Frank Hossfeld
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+package com.github.nalukit.nalu.plugin.domino.v2.client.component;
+
+import com.github.nalukit.nalu.client.component.AbstractCompositeController;
+import com.github.nalukit.nalu.client.context.IsContext;
+import org.dominokit.domino.ui.IsElement;
+
+public abstract class AbstractDominoV2CompositeController<C extends IsContext, V extends IsDominoV2CompositeComponent<?>>
+    extends AbstractCompositeController<C, V, IsElement<?>>
+    implements IsDominoV2CompositeComponent.Controller {
+}
