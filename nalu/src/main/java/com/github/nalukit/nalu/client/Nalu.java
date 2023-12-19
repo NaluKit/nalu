9c08a2059acf037652cbc362ba7892b682525090
Frank Hossfeld <frank.hossfeld@googlemail.com>
2023-12-19 16:41:34 +0100

    set version to HEAD-SNAPSHOT

--- nalu/src/main/java/com/github/nalukit/nalu/client/Nalu.java
+++ nalu/src/main/java/com/github/nalukit/nalu/client/Nalu.java
@@ -23,7 +23,7 @@ public class Nalu {
 
   public static String getVersion() {
     // TODO Change this for other versions
-    return "2.12.7";
+    return "HEAD-SNAPSHOT";
   }
 
   public static boolean hasHistory() {
