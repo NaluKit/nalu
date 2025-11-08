97fd4336e00017f0f3d175a73846bbfda92e2a11
Frank Hossfeld <frank.hossfeld@googlemail.com>
2025-11-08 11:33:33 +0100

    prepare release 3.1.1

--- nalu-processor/src/test/resources/io/github/nalukit/nalu/processor.templates/filter/eventhandler/eventHandlerOnAFilterOk02/ApplicationWithFilterImpl.java
+++ nalu-processor/src/test/resources/io/github/nalukit/nalu/processor.templates/filter/eventhandler/eventHandlerOnAFilterOk02/ApplicationWithFilterImpl.java
@@ -34,7 +34,7 @@ public final class ApplicationWithFilterImpl extends AbstractApplication<MockCon
     this.eventBus.fireEvent(LogEvent.create()
                                     .sdmOnly(true)
                                     .addMessage("=================================================================================")
-                                    .addMessage("Nalu processor version  >>HEAD-SNAPSHOT<< used to generate this source")
+                                    .addMessage("Nalu processor version  >>3.1.1<< used to generate this source")
                                     .addMessage("=================================================================================")
                                     .addMessage(""));
   }
--- nalu/src/main/java/io/github/nalukit/nalu/client/Nalu.java
+++ nalu/src/main/java/io/github/nalukit/nalu/client/Nalu.java
@@ -23,7 +23,7 @@ public class Nalu {
 
   public static String getVersion() {
     // TODO Change this for other versions
-    return "HEAD-SNAPSHOT";
+    return "3.1.1";
   }
 
   public static boolean hasHistory() {
